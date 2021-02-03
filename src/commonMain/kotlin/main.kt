e9254dca06f190f22a846008b532d6bc05d1b7ea
kabi_rules <javifont@gmail.com>
2021-02-03 22:53:50 +0100

    playing around with tilesets

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,10 +1,14 @@
 import com.soywiz.klock.*
+import com.soywiz.kmem.clamp
 import com.soywiz.korev.Key
 import com.soywiz.korge.*
+import com.soywiz.korge.tiled.readTiledMap
+import com.soywiz.korge.tiled.tiledMapView
 import com.soywiz.korge.view.*
 import com.soywiz.korim.color.Colors
 import com.soywiz.korim.format.*
 import com.soywiz.korio.file.std.*
+import kotlin.math.pow
 
 suspend fun main() = Korge(width = 640, height = 480, bgcolor = Colors["#2b2b2b"]) {
 	var idleLeftSpriteMap = resourcesVfs["Idle (78x58).png"].readBitmap().flipX()
@@ -60,6 +64,31 @@ suspend fun main() = Korge(width = 640, height = 480, bgcolor = Colors["#2b2b2b"
 			}
 		}
 	})
+	// TILESET
+	val tiledMap = resourcesVfs["sample.tmx"].readTiledMap()
+	fixedSizeContainer(256, 256, clip = true) {
+		position(128, 128)
+		val camera = camera {
+			tiledMapView(tiledMap) {
+			}
+		}
+		var dx = 0.0
+		var dy = 0.0
+		addUpdater {
+			//val scale = 1.0 / (it / 16.666666.hrMilliseconds)
+			val scale = if (it == 0.milliseconds) 0.0 else (it / 16.666666.milliseconds)
+			if (views.input.keys[Key.RIGHT]) dx -= 1.0
+			if (views.input.keys[Key.LEFT]) dx += 1.0
+			if (views.input.keys[Key.UP]) dy += 1.0
+			if (views.input.keys[Key.DOWN]) dy -= 1.0
+			dx = dx.clamp(-10.0, +10.0)
+			dy = dy.clamp(-10.0, +10.0)
+			camera.x += dx * scale
+			camera.y += dy * scale
+			dx *= 0.9.pow(scale)
+			dy *= 0.9.pow(scale)
+		}
+	}
 }
 
 
---
+++ src/commonMain/resources/-Overworld.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="Overworld" tilewidth="6" tileheight="4" tilecount="24" columns="6">
+    <image source="Terrain (32x32).png" width="608" height="416"/>
+</tileset>
