d92dc61321f47d7d1b2666f6f962f2de30824d4d
kabi_rules <javifont@gmail.com>
2021-02-04 22:12:40 +0100

    Box2D testing...

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -2,21 +2,29 @@ import com.soywiz.klock.*
 import com.soywiz.kmem.clamp
 import com.soywiz.korev.Key
 import com.soywiz.korge.*
+import com.soywiz.korge.box2d.registerBodyWithFixture
 import com.soywiz.korge.tiled.readTiledMap
 import com.soywiz.korge.tiled.tiledMapView
 import com.soywiz.korge.view.*
+import com.soywiz.korge.view.ktree.viewTreeToKTree
 import com.soywiz.korim.color.Colors
+import com.soywiz.korim.font.readBitmapFont
 import com.soywiz.korim.format.*
 import com.soywiz.korio.file.std.*
+import org.jbox2d.dynamics.BodyType
 import kotlin.math.pow
 
 suspend fun main() = Korge(width = 640, height = 480, bgcolor = Colors["#2b2b2b"]) {
+	val floor = resourcesVfs["floor.png"].readBitmap()
+	image(floor).position(250,300).registerBodyWithFixture(type=BodyType.KINEMATIC)
+
 	var idleLeftSpriteMap = resourcesVfs["Idle (78x58).png"].readBitmap().flipX()
 	var idleRightSpriteMap = resourcesVfs["Idle (78x58).png"].readBitmap()
 	var runLeftSpriteMap = resourcesVfs["Run (78x58).png"].readBitmap().flipX()
 	var runRightSpriteMap = resourcesVfs["Run (78x58).png"].readBitmap()
 	var attackLeftSpriteMap = resourcesVfs["Attack (78x58).png"].readBitmap().flipX()
 	var attackRightSpriteMap = resourcesVfs["Attack (78x58).png"].readBitmap()
+
 	val idleLeftAnimation = SpriteAnimation(
 		spriteMap = idleLeftSpriteMap,
 		spriteWidth = 78,
@@ -57,6 +65,7 @@ suspend fun main() = Korge(width = 640, height = 480, bgcolor = Colors["#2b2b2b"
 		scale(1)
 		position(256, 256)
 	}
+	sprite.registerBodyWithFixture(type = BodyType.DYNAMIC)
 	var lookingRight = true
 	sprite.playAnimationLooped(idleRightAnimation, spriteDisplayTime = 100.milliseconds)
 	val input = views.input
